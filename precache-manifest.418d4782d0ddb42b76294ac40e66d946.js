5887393ea5a60d0fd117998e9ba6f8e4f1fbee9e
zapata <snakamoshi@protonmail.com>
2021-02-24 23:18:55 +0800

    Update for topStat display

---
+++ precache-manifest.418d4782d0ddb42b76294ac40e66d946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d77d1d00e6bc7f9554a05f27469e9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79ab53d310fcd148fc64",
+    "url": "./static/css/main.a57d47b3.chunk.css"
+  },
+  {
+    "revision": "cadb49ea4e771add80bc",
+    "url": "./static/js/2.843d5ec6.chunk.js"
+  },
+  {
+    "revision": "c1396e93eed626ae26dccbcd9ba3e505",
+    "url": "./static/js/2.843d5ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ab53d310fcd148fc64",
+    "url": "./static/js/main.c2de1b7b.chunk.js"
+  },
+  {
+    "revision": "f7c634aa36e815b5c096",
+    "url": "./static/js/runtime-main.ac5d452d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.db8097bd115e0c5205e21a574798a04b.js"
+  "./precache-manifest.418d4782d0ddb42b76294ac40e66d946.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
